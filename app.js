a6614ce6c247778c4bda9d9e01be60d5b50987f1
shazadanjum2 <75418972+shazadanjum2@users.noreply.github.com>
2021-12-20 16:57:04 +0500

    initial

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
-import * as THREE from '/libs/three125/three.module.js';
-import { GLTFLoader } from '/libs/three/jsm/GLTFLoader.js';
-import { RGBELoader } from '/libs/three/jsm/RGBELoader.js';
-import { ARButton } from '/libs/ARButton.js';
-import { LoadingBar } from '/libs/LoadingBar.js';
+import * as THREE from '../../libs/three125/three.module.js';
+import { GLTFLoader } from '../../libs/three/jsm/GLTFLoader.js';
+import { RGBELoader } from '../../libs/three/jsm/RGBELoader.js';
+import { ARButton } from '../../libs/ARButton.js';
+import { LoadingBar } from '../../libs/LoadingBar.js';
 
 class App{
 	constructor(){
@@ -12,7 +12,7 @@ class App{
         this.loadingBar = new LoadingBar();
         this.loadingBar.visible = false;
 
-		this.assetsPath = '/assets/ar-shop/';
+		this.assetsPath = '../../assets/ar-shop/';
         
 		this.camera = new THREE.PerspectiveCamera( 70, window.innerWidth / window.innerHeight, 0.01, 20 );
 		this.camera.position.set( 0, 1.6, 0 );
@@ -52,12 +52,12 @@ class App{
 
 			navigator.xr.isSessionSupported( 'immersive-ar' ).then( ( supported ) => {
 
-                if (supported){
+               // if (supported){
                     const collection = document.getElementsByClassName("ar-button");
                     [...collection].forEach( el => {
                         el.style.display = 'block';
                     });
-                }
+                //}
 			} );
             
 		} 
